b07e212807c054526d416e7056e59c959396d0fd
Adistian Herlambang <adistian59@gmail.com>
2025-06-13 07:15:43 +0700

    initial

---
+++ main.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const cors = require('cors')
+const multer = require('multer')
+const TelegramBot = require('node-telegram-bot-api')
+const fs = require('fs')
+const path = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const upload = multer({ dest: 'uploads/' })
+
+const TELEGRAM_TOKEN = '7738409627:AAGxrG8H_LSyXppPea9t5zB268shh20Iuwo' // ganti dengan token bot Anda
+const CHAT_IDS_FILE = path.join(__dirname, 'chatid.json')
+
+const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true })
+
+// Helper functions for chat ID storage
+function readChatIds() {
+    try {
+        if (!fs.existsSync(CHAT_IDS_FILE)) return [];
+        const data = fs.readFileSync(CHAT_IDS_FILE, 'utf-8')
+        return JSON.parse(data)
+    } catch {
+        return []
+    }
+}
+function saveChatIds(chatIds) {
+    fs.writeFileSync(CHAT_IDS_FILE, JSON.stringify(chatIds, null, 2))
+}
+
+// Listen for /start and save chat id
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    const userName = msg.from.first_name;
+
+    let chatIds = readChatIds();
+
+    if (!chatIds.includes(chatId)) {
+        chatIds.push(chatId);
+        saveChatIds(chatIds); // chatId DISIMPAN KE FILE chatid.json
+    }
+
+    bot.sendMessage(chatId, `Assalamu'alaikum, ${userName}! Anda akan menerima laporan pengaduan di sini, Barakallahu fiikum.`);
+    console.log(`Chat ID: ${chatId}`);
+});
+
+app.use(cors ({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+
+app.post('/', upload.single('image'), async (req, res) => {
+    const data = req.body
+    const file = req.file
+
+    let message = `
+<b>Laporan Baru</b>
+Nama: ${data.nama}
+NIP: ${data.nip}
+Jenis: ${data.option}
+Tanggal & Lokasi: ${data.tanggalLokasi}
+Rincian: ${data.rincian}
+    `
+
+    try {
+        const chatIds = readChatIds();
+        // if (chatIds.length === 0) {
+        //     return res.status(400).json({ message: 'Belum ada chat id yang terdaftar. Kirim /start ke bot dulu.' });
+        // }
+        for (const chatId of chatIds) {
+            if (file) {
+                await bot.sendPhoto(chatId, fs.createReadStream(file.path), {
+                    caption: message,
+                    parse_mode: 'HTML'
+                })
+            } else {
+                await bot.sendMessage(chatId, message, { parse_mode: 'HTML' })
+            }
+        }
+        if (file) fs.unlinkSync(file.path) // hapus file setelah dikirim
+        // res.json({ message: 'Laporan berhasil dikirim!' })
+    } catch (err) {
+        console.error(err)
+        // res.status(500).json({ message: 'Gagal mengirim ke Telegram.' })
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`server berjalan di port: ${PORT}`)
+})
